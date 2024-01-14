6343f8340c1aedc3e08e54195094f3d8479ca894
Larry Finkelstein <larry.finkelstein@bms.com>
2024-01-14 13:23:03 -0500

    fix: Correct bucket name

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "Agithub-actions-larryf1-test"             # the bucket
+    bucket = "github-actions-larryf1-test"             # the bucket
     region = "us-east-1"             # the region
   }
 }
